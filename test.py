54886fbc82f95057ea249002de318c7d1fd62205
XXX <xxx@aa.com>
2023-03-14 19:58:28 +0800

    4hhh

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 # author: xdu_ptq time:2022/10/7
 print('hello world')
 def func():
-<<<<<<< HEAD
-    print("2hhh")
-=======
-    print("1hhh")
->>>>>>> 072aa18a81759c938a96d72084e051c1246b5476
-333
+    print("4hhh")
+
